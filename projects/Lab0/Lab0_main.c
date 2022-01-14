91057cfc4571f6a9a707ad8edeafd68505e32585
Robert <rbox@ucsc.edu>
2022-01-14 12:42:05 -0800

    comments + leds turn off when button release

--- projects/Lab0/Lab0_main.c
+++ projects/Lab0/Lab0_main.c
@@ -6,16 +6,12 @@
 //CSE13E Support Library
 #include "BOARD.h"
 
-// Microchip libraries
+#define PART1 1 // set to 1 for part1
+#define PART2 0 // set to 1 for part2
+#define NOPS_FOR_5MS 6000 // 6000 found based on the 1 minute counter
 
-#define PART1 0
-#define PART2 1
-#define NOPS_FOR_5MS 6000
-
-// **** Set any macros or preprocessor directives here ****
 
 void NOP_delay_5ms() {
-    
     int i; 
     for ( i =0; i < NOPS_FOR_5MS; i++) {
                 asm ("nop"); 
@@ -24,40 +20,45 @@ void NOP_delay_5ms() {
 
 int main()
 {
-    // initialize everything
     BOARD_Init();
    
-    TRISE = 0x00;
-    LATE = 0x00;
+    TRISE = 0x00; // sets leds to output mode
+    LATE = 0x00; // sets leds to off
     while (PART1) {
-        if (PORTFbits.RF1 == 1) {
+        if (PORTFbits.RF1 == 1) { // if a button is pressed an led will light up
             PORTEbits.RE0 = 1;
+        } else {
+            PORTEbits.RE0 = 0;
         }
         if (PORTDbits.RD5 == 1) {
             PORTEbits.RE1 = 1;
+        } else {
+            PORTEbits.RE1 = 0;
         }
         if (PORTDbits.RD6 == 1) {
             PORTEbits.RE2 = 1;
+        } else {
+            PORTEbits.RE2 = 0;
         }
         if (PORTDbits.RD7 == 1) {
             PORTEbits.RE3 = 1;
+        } else {
+            PORTEbits.RE3 = 0;
         }
     }
     
     while (PART2) {
-        PORTE = 0x00;
+        PORTE = 0x00; // initialize count to zero
         while (1) {
             PORTE++;
             int i; 
-            for ( i =0; i < 50; i++) {
-                if (PORTFbits.RF1 == 1) {
+            for ( i =0; i < 50; i++) { // checks every 5ms for a button press
+                // loop lasts 250ms
+                if (PORTFbits.RF1 == 1) { // if button pressed reset count
                 PORTE = 0x00;
                 }
                 NOP_delay_5ms();
             }
         }
     }
-
-    //initialize state machine (and anything else you need to init) here
-
 }

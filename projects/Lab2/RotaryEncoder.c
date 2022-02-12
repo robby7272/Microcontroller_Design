aa2183b7440a1b09038e43b3893a91981dbe2516
Robert Dominik Box <rbox@ucsc.edu>
2022-02-12 06:58:26 +0000

    Update RotaryEncoder.c

--- projects/Lab2/RotaryEncoder.c
+++ projects/Lab2/RotaryEncoder.c
@@ -12,8 +12,9 @@
 #define ENCODER_BLOCKING_MODE 0
 #define ENCODER_INTERRUPT_MODE 1
 
-#define CS LATDbits.LATD4 // pin 10
+#define CS LATFbits.LATF1 // pin 4
 unsigned short rData;
+unsigned short error;
 /*******************************************************************************
  * PUBLIC FUNCTIONS                                                           *
  ******************************************************************************/
@@ -37,7 +38,7 @@ int RotaryEncoder_Init(char interfaceMode) {
     SPI2CONbits.CKP = 0; // clock idle low
     SPI2CONbits.ON = 1;     // SPI ON
     
-    TRISDbits.TRISD4 = 0; // output mode for CS
+    TRISFbits.TRISF1 = 0; // output mode for CS
     CS = 1; // slave I/O pin
 
     // SPI2BUF = 'A'
@@ -49,6 +50,7 @@ int RotaryEncoder_Init(char interfaceMode) {
  * @param None
  * @return 14-bit number representing the raw encoder angle (0-16384) */
 unsigned short RotaryEncoder_ReadRawAngle(void) {
+    //return (rData & 0b0100000000000000) >> 14;
     return rData & 0b0011111111111111;
 }
 
@@ -66,43 +68,38 @@ unsigned int parityCheck(unsigned int in) {
     return p;
 }
 
-void delay(void) {
-    
-    unsigned int time = 0;
-    unsigned int time2 = 0;
-    time = FreeRunningTimer_GetMilliSeconds();
-    while(1) {
-        time2 = FreeRunningTimer_GetMilliSeconds();
-        if (time+10 < time2) {
-            break;
-        }
+void delay1mS(void) {
+    unsigned int i;
+    for (i = 0; i < 5; i++) {
+        asm("nop");
     }
-    
 }
 
-#define sendToEncoder
+//#define sendToEncoder
 #ifdef sendToEncoder
 int main() {
-    unsigned short dData;
     BOARD_Init();
     Protocol_Init();
     FreeRunningTimer_Init();
     RotaryEncoder_Init(ENCODER_BLOCKING_MODE);
     unsigned short NOP = 0xC000;
-    unsigned short packet = 0x7FFE;
+    unsigned short packet = 0x7FFE; // measured angle
     
     
     while(1) {
-        delay();
+        delay1mS();
         CS = 0;
         SPI2BUF = packet; // copy packet into appropriate buffer
-        while(SPI2STATbits.SPIBUSY == 1); // packet transfer in progress
+        while(!SPI2STATbits.SPIRBF); // packet transfer in progress
         CS = 1;
-        rData=SPI2BUF; // read data
-        rData = RotaryEncoder_ReadRawAngle();
+        rData=SPI2BUF; // read data       
+        
+        rData = rData & 0b0011111111111111;
+        error = (rData & 0b0100000000000000) >> 14;
         char timerMessage[MAXPAYLOADLENGTH];
-        sprintf(timerMessage, "%u", rData);
+        sprintf(timerMessage, "Angle: %u Error: %u", rData, error);
         Protocol_SendDebugMessage(timerMessage);
+        //Protocol_SendMessage(4, 0x86, &rData);
         
     }
 }

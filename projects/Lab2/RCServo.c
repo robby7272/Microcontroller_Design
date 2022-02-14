efba5d72e737a9e0e8e9e03e17537f9f5a535c68
Robert Dominik Box <rbox@ucsc.edu>
2022-02-14 07:04:34 +0000

    need to finish test harness

--- projects/Lab2/RCServo.c
+++ projects/Lab2/RCServo.c
@@ -11,15 +11,19 @@
 #include "Protocol.h"
 
 
-#define RC_SERVO_MIN_PULSE 600
-#define RC_SERVO_CENTER_PULSE 1500
-#define RC_SERVO_MAX_PULSE 2400
+//preset 64
+#define RC_SERVO_MIN_PULSE 625 // 1 millisecond
+#define RC_SERVO_CENTER_PULSE 938 // 1.5 milliseconds
+#define RC_SERVO_MAX_PULSE 1250 // 2 milliseconds
 
 /*******************************************************************************
  * PUBLIC FUNCTIONS                                                           *
  ******************************************************************************/
 #define TRIGGER LATFbits.LATF1 // pin 4
+#define OUTPUTCAPTURE LATDbits.LATD2 // pin 6
 unsigned int j;
+unsigned int tickPulse = RC_SERVO_CENTER_PULSE;
+unsigned int microsPulse;
 
 /**
  * @Function RCServo_Init(void)
@@ -38,6 +42,14 @@ int RCServo_Init(void) {
     T3CONbits.ON = 1; // Start timer
     
 
+    OC3CON = 0x0; // clear control registers
+    OC3CONbits.OCTSEL = 1; // select Timer3
+    OC3CONbits.OCM = 0b110; // PWM, no fault pin
+    OC3RS = RC_SERVO_CENTER_PULSE; // duty cycle, controlled by user
+    OC3R = 900; // initial value before RS is copied in
+    
+    OC3CONbits.ON = 1; // turn output capture on
+            
     TRISFbits.TRISF1 = 0; // output
     TRIGGER = 0;
 }
@@ -48,37 +60,58 @@ int RCServo_Init(void) {
  * @return SUCCESS or ERROR
  * @brief takes in microsecond count, converts to ticks and updates the internal variables
  * @warning This will update the timing for the next pulse, not the current one */
-int RCServo_SetPulse(unsigned int inPulse);
+int RCServo_SetPulse(unsigned int inPulse) {
+    unsigned int outPulse = 0;
+    microsPulse = inPulse;
+    outPulse = inPulse / 8;
+    outPulse = outPulse * 5; // multiplies by 0.625 to convert to ticks
+    if (outPulse > RC_SERVO_MAX_PULSE) {
+        outPulse = RC_SERVO_MAX_PULSE;
+    }
+    if (outPulse < RC_SERVO_MIN_PULSE) {
+        outPulse = RC_SERVO_MIN_PULSE;
+    }
+    tickPulse = outPulse;
+    
+}
 
 /**
  * @Function int RCServo_GetPulse(void)
  * @param None
  * @return Pulse in microseconds currently set */
-unsigned int RCServo_GetPulse(void);
+unsigned int RCServo_GetPulse(void) {
+    return microsPulse;
+}
 
 /**
  * @Function int RCServo_GetRawTicks(void)
  * @param None
  * @return raw timer ticks required to generate current pulse. */
-unsigned int RCServo_GetRawTicks(void);
+unsigned int RCServo_GetRawTicks(void) {
+    return tickPulse;
+}
 
 
 void __ISR(_TIMER_3_VECTOR) Timer3IntHandler(void) {
     IFS0CLR = 0x00001000; // clear Timer3 interrupt flag
-    TRIGGER = 1;
-    for (j = 0; j < 12; j++) { // 10 microseconds
-        asm("nop");
-    }
-    TRIGGER = 0;
 }
+
 
 #define timer
 #ifdef timer
 int main() {
     BOARD_Init();
-    //FreeRunningTimer_Init();
-    //Protocol_Init();
+    FreeRunningTimer_Init();
+    Protocol_Init();
     RCServo_Init();
-    while(1);
+    
+    unsigned short Data = 0;
+    unsigned short prevData = 0;
+    unsigned int num = 0;
+    while(1) {
+        Protocol_GetPayload(&Data);
+        Protocol_SendMessage(4, 0x89, &Data);
+
+    }
 }
 #endif

c61b03a0bd730bb3c1fbcd06c28c453c0bd46981
Robert Dominik Box <rbox@ucsc.edu>
2022-02-25 02:48:21 +0000

    init I2C

--- projects/Lab3/NonVolatileMemory.c
+++ projects/Lab3/NonVolatileMemory.c
@@ -1,3 +1,6 @@
+#include "NonVolatileMemory.h"
+#include "BOARD.h"
+
 /*******************************************************************************
  * PUBLIC #DEFINES                                                             *
  ******************************************************************************/
@@ -11,7 +14,12 @@
  * @param None
  * @return SUCCESS or ERROR
  * @brief initializes I2C for usage */
-int NonVolatileMemory_Init(void);
+int NonVolatileMemory_Init(void) {
+    I2C1CON = 0; // clear control bits
+    I2C1BRG = 0xC2; // 100 KHz
+    I2C1CONbits.DISSLW = 1; // Disable slew rate
+    I2C1CONbits.ON = 1; // turn on
+}
 
 /**
  * @Function NonVolatileMemory_ReadByte(int address)
@@ -27,7 +35,38 @@ unsigned char NonVolatileMemory_ReadByte(int address);
  * @param data, value to write at said address
  * @return SUCCESS or ERROR
  * @brief writes one byte to device */
-char NonVolatileMemory_WriteByte(int address, unsigned char data);
+char NonVolatileMemory_WriteByte(int address, unsigned char data) {
+    
+    I2C1CONbits.SEN = 1; // Send Start bit
+    while(I2C1CONbits.SEN == 1); // wait for Send transmission to be over
+    
+    I2C1TRN = 0b10100000;
+    while(I2C1STATbits.TBF == 1); // wait for Transmit buffer 
+    
+    I2C1CONbits.ACKEN = 1; // acknowledge event
+    while(I2C1STATbits.ACKSTAT == 1); // wait for ACK transmission
+    
+    I2C1TRN = (char) address >> 8; // high bits
+    while(I2C1STATbits.TBF == 1); // wait for Transmit buffer 
+    
+    I2C1CONbits.ACKEN = 1; // acknowledge event
+    while(I2C1STATbits.ACKSTAT == 1); // wait for ACK transmission
+    
+    I2C1TRN = (char) address; // low bits
+    while(I2C1STATbits.TBF == 1); // wait for Transmit buffer 
+    
+    I2C1CONbits.ACKEN = 1; // acknowledge event
+    while(I2C1STATbits.ACKSTAT == 1); // wait for ACK transmission
+    
+    I2C1TRN = data; // low bits
+    while(I2C1STATbits.TBF == 1); // wait for Transmit buffer 
+    
+    I2C1CONbits.ACKEN = 1; // acknowledge event
+    while(I2C1STATbits.ACKSTAT == 1); // wait for ACK transmission
+    
+    I2C1CONbits.PEN = 1; // Send Stop bit
+    while(I2C1CONbits.PEN == 1); // wait for stop transmission
+}
 
 /**
  * @Function int NonVolatileMemory_ReadPage(int page, char length, unsigned char data[])
@@ -48,3 +87,6 @@ int NonVolatileMemory_ReadPage(int page, char length, unsigned char data[]);
 int NonVolatileMemory_WritePage(int page, char length, unsigned char data[]);
 
 
+int main() {
+    while(1);
+}

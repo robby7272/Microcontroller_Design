417ebe646b659534b1e28f8bcd0431f8f71cffe4
Robert Dominik Box <rbox@ucsc.edu>
2022-03-08 03:21:41 +0000

    init and functions

--- projects/Lab3/ADCFilter.c
+++ projects/Lab3/ADCFilter.c
@@ -4,6 +4,7 @@
 #include "Protocol.h"
 #include <stdio.h>
 #include "ADCFilter.h"
+#include "MessageIDs.h"
 /*******************************************************************************
  * PUBLIC #DEFINES                                                             *
  ******************************************************************************/
@@ -17,6 +18,14 @@
 /*******************************************************************************
  * PUBLIC FUNCTION PROTOTYPES                                                  *
  ******************************************************************************/
+
+short data[4][FILTERLENGTH]; // initialized to 0's
+short filters[4][FILTERLENGTH]; // initialized to 0's
+unsigned int Head = -1;
+short test;
+unsigned char prevChannel = -1;
+unsigned char currChannel = 0;
+unsigned short prevValue;
 
 /**
  * @Function ADCFilter_Init(void)
@@ -41,7 +50,7 @@ int ADCFilter_Init(void) {
     
     AD1CON2bits.CSCNA = 1; // Scan mode on
     AD1CON2bits.VCFG = 0b000; // internal voltage reference
-    AD1CON2bits.SMPI = 0b000; // interrupt after each measurement
+    AD1CON2bits.SMPI = 0b011; // interrupt after 4th measurement
     
     AD1CON3bits.ADRC = 0; // peripheral bus clock
     AD1CON2bits.BUFM = 0; // 16 bit buffer
@@ -49,12 +58,15 @@ int ADCFilter_Init(void) {
     AD1CON3bits.ADCS = 0b10101101; // Tad = 348 Tpb
     AD1CON3bits.SAMC = 0b10000; // Sample time 16 Tad
     
+    AN2 = 1;
+    AN4 = 1;
+    AN8 = 1;
+    AN10 = 1;
+    
     AD1CON1bits.ON = 1; // turn on
     IFS1bits.AD1IF = 0; // clear interrupt
+    IPC6bits.AD1IP = 4; // interrupt priority
     IEC1bits.AD1IE = 1; // enable interrupt
-    
-    // turn on timer?
-    
 }
 
 /**
@@ -62,14 +74,18 @@ int ADCFilter_Init(void) {
  * @param pin, which channel to return
  * @return un-filtered AD Value
  * @brief returns current reading for desired channel */
-short ADCFilter_RawReading(short pin);
+short ADCFilter_RawReading(short pin) {
+    return data[pin][Head];
+}
 
 /**
  * @Function ADCFilter_FilteredReading(short pin)
  * @param pin, which channel to return
  * @return Filtered AD Value
  * @brief returns filtered signal using weights loaded for that channel */
-short ADCFilter_FilteredReading(short pin);
+short ADCFilter_FilteredReading(short pin) {
+    return ADCFilter_ApplyFilter(filters[pin], data[pin], Head);
+}
 
 /**
  * @Function short ADCFilter_ApplyFilter(short filter[], short values[], short startIndex)
@@ -79,7 +95,16 @@ short ADCFilter_FilteredReading(short pin);
  * @return Filtered and Scaled Value
  * @brief returns final signal given the input arguments
  * @warning returns a short but internally calculated value should be an int */
-short ADCFilter_ApplyFilter(short filter[], short values[], short startIndex);
+short ADCFilter_ApplyFilter(short filter[], short values[], short startIndex) {
+    int i = 0;
+    unsigned int sum = 0;
+    short count = startIndex;
+    for (i = 0; i < FILTERLENGTH; i++) {
+        sum += filter[i] * values[count];
+        count = (count+1)%FILTERLENGTH;
+    }
+    return (short) sum >> 15;
+}
 
 /**
  * @Function ADCFilter_SetWeights(short pin, short weights[])
@@ -87,14 +112,32 @@ short ADCFilter_ApplyFilter(short filter[], short values[], short startIndex);
  * @param pin, array of shorts to load into the filter for the channel
  * @return SUCCESS or ERROR
  * @brief loads new filter weights for selected channel */
-int ADCFilter_SetWeights(short pin, short weights[]);
+int ADCFilter_SetWeights(short pin, short weights[]) {
+    int i = 0;
+    for (i = 0; i < FILTERLENGTH; i++) {
+        filters[pin][i] = weights[i];
+    }
+    return 1;
+}
 
 
 
 void __ISR(_ADC_VECTOR) ADCIntHandler(void) {
-IFS1bits.AD1IF = 0; // clear interrupt
+    IFS1bits.AD1IF = 0; // clear interrupt
+    
+    Head += 1;
+    if  (Head == FILTERLENGTH) {
+        Head = 0;
+    }
+    
+    data[0][Head] = ADC1BUF0;
+    data[1][Head] = ADC1BUF1;
+    data[2][Head] = ADC1BUF2;
+    data[3][Head] = ADC1BUF3;
 }
 
+#define filterTest
+#ifdef filterTest
 int main() {
     BOARD_Init();
     LEDS_INIT();
@@ -102,5 +145,39 @@ int main() {
     FreeRunningTimer_Init();
     ADCFilter_Init();
     
-    while(1);
+    
+    unsigned short filterValues[FILTERLENGTH];
+    unsigned int time1 = 0;
+    unsigned int time2 = 0;
+    
+    char debugMessage[MAXPAYLOADLENGTH];
+    sprintf(debugMessage, "Protocol Test Compiled at %s %s", __DATE__, __TIME__);
+    Protocol_SendDebugMessage(debugMessage);
+    
+    while(1) {
+        if (Protocol_ReadNextID() == ID_ADC_SELECT_CHANNEL) {
+            Protocol_GetPayload(&currChannel);
+            if (prevChannel != currChannel) {
+                Protocol_SendMessage(1, ID_ADC_SELECT_CHANNEL_RESP, &currChannel);
+                prevChannel = currChannel;
+            }
+        }
+        if (Protocol_ReadNextID() == ID_ADC_FILTER_VALUES) {
+            Protocol_GetPayload(&filterValues);
+            if (prevValue != filterValues[0]) {
+                int i = 0;
+                for (i = 0; i < FILTERLENGTH; i++) {
+                    filterValues[i] = Protocol_ShortEndednessConversion(filterValues[i]);
+                }
+                ADCFilter_SetWeights(currChannel, filterValues);
+                Protocol_SendMessage(1, ID_ADC_FILTER_VALUES_RESP, &currChannel);
+                filterValues[0] = Protocol_ShortEndednessConversion(filterValues[0]);
+                prevValue = filterValues[0];
+            }
+            
+        }
+        
+        
+    }
 }
+#endif
